281d74556e9f97296fe1214710d8588f665d0865
yuki3738 <y.minamiya@gmail.com>
2018-03-22 01:05:42 +0900

    Change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
   resources :users
+  get '/hello', to: 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
